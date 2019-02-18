14d483608ed80bdf5f0abb47a825ff26b044d918
lugp <18052601789@163.com>
2019-02-18 14:59:55 +0800

    update

---
+++ src/main/java/com/lgpgit/open/fbms/FbmsApplication.java
@@ -0,0 +1,13 @@
+package com.lgpgit.open.fbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbmsApplication.class, args);
+	}
+
+}
